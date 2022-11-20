0f63dfffc5f42fd7d81d688b76a8ddfd99b20bda
Ted <oancea.tudor@icloud.com>
2022-11-20 14:14:49 +0100

    worked on find_cones\(\)

--- fsds_client/high_level_client.py
+++ fsds_client/high_level_client.py
@@ -3,7 +3,7 @@ import time
 from typing import Callable, Optional
 
 import numpy as np
-
+from scipy.spatial.transform import Rotation as R
 from .low_level_client import LowLevelClient
 from .types import *
 
@@ -37,13 +37,13 @@ def crash_guard(callback: Callable, error_message: str, aux: Optional[Callable] 
 
 
 class HighLevelClient:
+    _delta_max: float = np.deg2rad(35.0)
+
     def __init__(
         self,
         ip: str = "127.0.0.1",
-        max_steering: float = 30.0,
         camera_name: str = "examplecam",
     ):
-        self.max_steering = max_steering
         self.camera_name = camera_name
 
         self.client = LowLevelClient(ip, 41451, 3)
@@ -52,100 +52,121 @@ class HighLevelClient:
 
         crash_guard(self.client.restart, "restart")
 
-        sleep_sub_ms(1)
+        sleep_sub_ms(3)
 
-        crash_guard(
-            lambda: self.client.enableApiControl(True),
-            "enableAPIControl",
-            self.client.restart,
-        )
+        # crash_guard(
+        #     lambda: self.client.enableApiControl(True),
+        #     "enableAPIControl",
+        #     self.client.restart,
+        # )
 
         self.image = None
 
         self.state = None
 
         self.update_state()
+        # self.state = self.client.simGetGroundTruthKinematics()
+
+    # utils ==================================================================
+
+    def available(self) -> bool:
+        return self.client.ping()
 
     def update_state(self):
         """Get the current state of the vehicle"""
         self.state = self.client.simGetGroundTruthKinematics()
 
-    def calculate_throttle_brake(self, throttle: float):
-        """
-        Calculate the throttle and brake values from the throttle input.
-        """
-        if throttle > 0:
-            return throttle, 0
-        else:
-            return 0, throttle
-
-    def calculate_steering(self, steer: float) -> float:
-        """Steer is in radians and in [-max_steering, max_steering]"""
-        control_steering = -steer / np.deg2rad(self.max_steering)
-        return control_steering
-
-    def yaw_rate(self) -> float:
-        """
-        Yaw rate of the vehicle in radians per second (anti-clockwise).
-        """
-        return self.state.angular_velocity.z_val
-
-    def yaw(self) -> float:
-        """
-        Yaw of the vehicle in radians.
-        Convention: The 0 rads line is the X axis and the yaw is measured anti-clockwise.
-        """
+    # interact with the simulation ===========================================
+    def get_state(self) -> np.ndarray:
+        """Get the current state of the vehicle in the following order: [X,Y,phi,v_x,v_y,r]"""
+        self.state = self.client.simGetGroundTruthKinematics()
+        # position
+        x = self.state.position.x_val
+        y = self.state.position.y_val
+        # orientation
         qx = self.state.orientation.x_val
         qy = self.state.orientation.y_val
         qz = self.state.orientation.z_val
         qw = self.state.orientation.w_val
-
-        yaw = (
-            np.mod(
-                np.arctan2(2 * (qw * qz + qx * qy), 1 - 2 * (qy * qy + qz * qz))
-                + np.pi / 2
-                + np.pi,
-                2 * np.pi,
-            )
-            - np.pi
-        )
-        return yaw
-
-    def speed(self) -> float:
-        """
-        Speed of the vehicle in the referential of the track.
-        """
-        return np.hypot(
-            self.state.linear_velocity.x_val, self.state.linear_velocity.y_val
-        )
-
-    def speed_longitudinal(self) -> float:
-        """
-        Speed of the vehicle in the direction of the car.
-        """
+        phi = np.mod(quaternion_to_euler(qw, qx, qy, qz)[2] + np.pi, 2 * np.pi) - np.pi
+        # linear velocity
+        v = np.hypot(self.state.linear_velocity.x_val, self.state.linear_velocity.y_val)
         angle_vx_vy = np.arctan2(
-            self.state.linear_velocity.x_val, -self.state.linear_velocity.y_val
+            self.state.linear_velocity.y_val, self.state.linear_velocity.x_val
         )
-        return self.speed() * np.cos(angle_vx_vy - self.yaw())
+        v_x = v * np.cos(angle_vx_vy - phi)
+        v_y = v * np.sin(angle_vx_vy - phi)
+        # angular velocity
+        r = self.state.angular_velocity.z_val
 
-    def available(self) -> bool:
-        return self.client.ping()
+        return np.array([x, y, phi, v_x, v_y, r])
 
-    def get_states(self) -> np.ndarray:
+    def send_control(self, T: float, delta: float):
         """
-        Get the current state of the vehicle.
+        :param T: Throttle value between -1 and 1.
+        :param delta: Steering angle in radians between -max_steering and max_steering.
         """
-        x = self.x()
-        y = self.y()
-        yaw = self.yaw()
-        yaw_rate = self.yaw_rate()
-        speed = self.speed_longitudinal()
+        car_controls = CarControls()
+        car_controls.steering = -delta / np.deg2rad(
+            self._delta_max
+        )  # delta is anti-clockwise but steering is clockwise
+        if T > 0:
+            car_controls.throttle, car_controls.brake = T, 0
+        else:
+            car_controls.throttle, car_controls.brake = 0, T
 
-        return np.array([x, y, yaw, yaw_rate, speed])
+        self.client.setCarControls(car_controls)
 
-    def update_image(self):
+    def find_cones(
+        self, max_cone_distance: float = 20.0, max_group_dispersion: float = 0.2
+    ) -> tuple[np.ndarray, np.ndarray]:
         """
-        Get the image string from the simulation.
+        Simple lidar cone detection algorithm that returns the positions of the cones in
+        a local cartesian frame, i.e. X is forward, Y is left.
+        """
+        # Get the pointcloud
+        lidardata = self.client.getLidarData()
+        if len(lidardata.point_cloud) < 3:
+            # not enough points
+            return np.array([]), np.array([])
+
+        # Convert the list of floats into a list of xyz coordinates
+        points = np.array(lidardata.point_cloud, dtype=np.dtype("f4"))
+        points = np.reshape(points, (int(points.shape[0] / 3), 3))
+        # find altitude of the cones and discard those that are too low (below 0.05) or too high (above 0.7)
+        qx = lidardata.pose.orientation.x_val
+        qy = lidardata.pose.orientation.y_val
+        qz = lidardata.pose.orientation.z_val
+        qw = lidardata.pose.orientation.w_val
+        lidar_orientation = R.from_quat([qx, qy, qz, qw])
+        altitudes = (
+            lidar_orientation.apply(points)[:, 2]
+            # R.from_quat([qx, qy, qz, qw]).inv().apply(points)[:, 2]
+            + lidardata.pose.position.z_val
+        )
+        print("min altitude: ", np.min(altitudes))
+        print("max altitude: ", np.max(altitudes))
+        old_size = points.shape[0]
+        points = points[
+            (altitudes > 0.1) & (altitudes < 0.7) & (np.linalg.norm(points, axis=1) < max_cone_distance)
+        ]
+        print("points removed: ", old_size - points.shape[0])
+        # only keep X and Y coordinates and discard the cones that are too far
+        points = points[:, :2]
+        # points = points[np.linalg.norm(points, axis=1) < max_cone_distance]
+
+        # Go through all the points and find nearby groups of points that are close
+        # together as those will probably be cones.
+        distances = np.sqrt(np.sum(np.square(np.diff(points, axis=0)), axis=1))
+        groups = np.split(points, np.where(distances > max_group_dispersion)[0] + 1)
+        cones = np.array([np.mean(group, axis=0) for group in groups])
+
+        return cones, points
+
+    def get_image(self) -> np.ndarray:
+        """
+        Get the current image from the camera.
+        :return: The image as a numpy array of shape (height, width, 3).
         """
         [self.image] = self.client.simGetImages(
             [
@@ -157,79 +178,34 @@ class HighLevelClient:
                 )
             ]
         )
-
-    def find_cones(self):
-        # Get the pointcloud
-        lidardata = self.client.getLidarData(lidar_name="Lidar")
-
-        # no points
-        if len(lidardata.point_cloud) < 3:
-            return []
-
-        # Convert the list of floats into a list of xyz coordinates
-        points = np.array(lidardata.point_cloud, dtype=np.dtype("f4"))
-        points = np.reshape(points, (int(points.shape[0] / 3), 3))
-
-        # Go through all the points and find nearby groups of points that are close together as those will probably be cones.
-
-        current_group = []
-        cones = np.ndarray(shape=(0, 2))
-        for i in range(1, len(points)):
-
-            # Get the distance from current to previous point
-            distance_to_last_point = distance(
-                points[i][0], points[i][1], points[i - 1][0], points[i - 1][1]
-            )
-
-            if distance_to_last_point < 0.1:
-                # Points closer together then 10 cm are part of the same group
-                current_group.append(np.array([points[i][0], points[i][1]]))
-            else:
-                # points further away indiate a split between groups
-                if len(current_group) > 0:
-                    cone = pointgroup_to_cone(current_group)
-                    # calculate distance between lidar and cone
-                    if distance(0, 0, cone[0], cone[1]) < 7.0:
-                        cones = np.vstack((cones, cone))
-                    current_group = []
-
-        return cones
-
-    def get_image(self) -> np.ndarray:
-        """
-        Get the current image from the camera.
-        :return: The image as a numpy array of shape (height, width, 3).
-        """
         img1d = np.fromstring(self.image.image_data_uint8, dtype=np.uint8)
         img_rgb = img1d.reshape((self.image.height, self.image.width, 3))
 
         return img_rgb
 
-    def x(self) -> float:
-        """
-        First global cartesian coordinate.
-        Convention: At the beginning of the simulation the X-axis is oriented from left to right of the car.
-        """
-        return -self.state.position.y_val
 
-    def y(self) -> float:
-        """
-        Second global cartesian coordinate.
-        Convention: At the beginning of the simulation the Y-axis is oriented in the same as the car.
-        """
-        return self.state.position.x_val
+def quaternion_to_euler(w, x, y, z):
+    """Convert a quaternion to euler angles (roll, pitch, yaw) in radians"""
+    # ysqr = y * y
+    #
+    # t0 = +2.0 * (w * x + y * z)
+    # t1 = +1.0 - 2.0 * (x * x + ysqr)
+    # roll = np.arctan2(t0, t1)
+    #
+    # t2 = +2.0 * (w * y - z * x)
+    # t2 = np.where(t2 > +1.0, +1.0, t2)
+    # # t2 = +1.0 if t2 > +1.0 else t2
+    #
+    # t2 = np.where(t2 < -1.0, -1.0, t2)
+    # # t2 = -1.0 if t2 < -1.0 else t2
+    # pitch = np.arcsin(t2)
+    #
+    # t3 = +2.0 * (w * z + x * y)
+    # t4 = +1.0 - 2.0 * (ysqr + z * z)
+    # yaw = np.arctan2(t3, t4)
+    # return roll, pitch, yaw
 
-    def send_control(self, steering: float, throttle: float):
-        """
-        Send control to the vehicle.
-        Steering is in [-max_steering, max_steering], throttle is in [-1, 1]
-        """
-        car_controls = CarControls()
-        car_controls.steering = self.calculate_steering(steering)
-        car_controls.throttle, car_controls.brake = self.calculate_throttle_brake(
-            throttle
-        )
-        self.client.setCarControls(car_controls)
+    return R.from_quat([x, y, z, w]).as_euler("xyz", degrees=False)
 
 
 def sleep_sub_ms(delay):
@@ -241,14 +217,3 @@ def sleep_sub_ms(delay):
 
 def distance(x1, y1, x2, y2):
     return np.sqrt(np.abs(x1 - x2) ** 2 + np.abs(y1 - y2) ** 2)
-
-
-def pointgroup_to_cone(group):
-    average_x = 0
-    average_y = 0
-    for point in group:
-        average_x += point[0]
-        average_y += point[1]
-    average_x = average_x / len(group)
-    average_y = average_y / len(group)
-    return np.array([average_x, average_y])
